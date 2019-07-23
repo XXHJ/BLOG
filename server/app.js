b599e601cc31abe2d85ea700a7afc03cb649d7e2
xiong.xiao <xiong.xiao@aisppeech.com>
2019-07-23 19:14:46 +0800

    fix:更新session存储

--- server/app.js
+++ server/app.js
@@ -9,23 +9,24 @@ const app = express()
 
 const resolve = file => path.resolve(__dirname, file)
 
-app.use('/dist', express.static(resolve('./dist')))
+app.use('/dist', express.static(resolve('../dist')))
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
 app.use(router)
 
 // session
-app.set('trust proxy', 1) // trust first proxy
+// app.set('trust proxy', 1) // trust first proxy
+
 app.use(session({
-  secret: 'blog',
-  resave: false,
-  saveUninitialized: true,
+  name: 'blog', // 设置 cookie 中保存 session id 的字段名称
+  secret: 'blog', // 通过设置 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true, // 强制更新 session
+  saveUninitialized: false, // 设置为 false，强制创建一个 session，即使用户未登录
   cookie: {
-    secure: true,
-    maxAge: 2592000000
+    maxAge: 2592000000// 过期时间，过期后 cookie 中的 session id 自动删除
   },
-  store: new MongoStore({
-    url: 'mongodb://localhost:27017/blog'
+  store: new MongoStore({// 将 session 存储到 mongodb
+    url: 'mongodb://localhost:27017/blog' // mongodb 地址
   })
 }))
 
